76ad3f66f9b51f5487e48d4eb027fe0d68381be0
Roshan karki <rohitkc8848@gmail.com>
2023-05-17 02:01:47 +0545

    feat: wrapping up the todos project

--- todos/index.js
+++ todos/index.js
@@ -1,95 +1,104 @@
 const formInput=document.querySelector('.inputwala')
+// edit
+const editInput=document.querySelector('.editinput')
+const closebtn=document.querySelector('.btn-close')
+const savechangesbtn=document.querySelector('.changebtn')
+let changeid=""
+// edit ends 
 const btn=document.querySelector('.submit-btn')
 const todosList=document.querySelector('.todos-ordered-list')
+
 let todos=[]
-let i=0;
+
 btn.addEventListener('click',(e)=>{
     e.preventDefault()
-    todos.push({value:formInput.value,id:Date.now()})
-
-    todos.map((todo,index)=>{
-        if(i===index){
-
-            const li=document.createElement('li')
-            li.classList.add("list-group-item","p-4",'bg-info',"d-flex", "align-content-center","justify-content-between")
-            const burronwrapper=document.createElement('div')
-            
-            // task
-            const task=document.createElement('span')
-        task.innerText=todo.value
-        // update button
-        const updatebutton=document.createElement('button')
-        updatebutton.innerHTML="update"
-        updatebutton.classList.add('btn','btn-primary','mx-3')
-        updatebutton.onclick=()=>{deleteme(todo.id)}
-        
-        const deletebutton=document.createElement('button')
-        deletebutton.innerHTML="delete"
-        deletebutton.classList.add('btn','btn-primary')
-        // wrapping up the buttons in div
-        burronwrapper.append(updatebutton)
-        burronwrapper.append(deletebutton)
-        
-        // making the li
-        li.append(task)
-        li.append(burronwrapper)
-        li=deletebutton.onclick=()=>{deleteme(li)}
-        todosList.append(li)
-        console.log(todos);
-        // todos=[]
+    const todo={
+        id:Date.now(),
+        name:formInput.value,
+        status:false
     }
-    })
-    i++
-   console.log(i);
+    todos.push(todo)
     formInput.value=""
+    displayTodos()
+    console.log(todos);
+})
+function displayTodos(){
+    todosList.innerHTML=""
+    todos.map((todo)=>{
+        // creating the main wrapper li
+        const li=document.createElement('li')
+        const wrapme=document.createElement('div')
+        li.classList.add('list-group-item','p-3', 'd-flex', 'justify-content-between')
+        // checkbox
+        const checkbox=document.createElement('input')
+        checkbox.classList.add('mx-2')
+        checkbox.type="checkbox"
+        checkbox.checked =todo.status
+        checkbox.addEventListener('change',(e)=>{
+            e.preventDefault()
+            console.log(e.target.checked);
+            todo.status=e.target.checked
+            displayTodos()
+        })
+       
+        // creating the span 
+        const taskadder=document.createElement('span')
+        taskadder.style.textDecoration = todo.status ? 'line-through' : 'none';
+        taskadder.innerHTML=todo.name
+        wrapme.append(checkbox)
+        wrapme.append(taskadder)
+        
+    
+    // creating the button 
+    const buttonWrapper=document.createElement('div')
+    // updated button
+    const updateButton=document.createElement('button')
+    updateButton.innerHTML="update me"
+    updateButton.classList.add('btn',"btn-primary",'me-3')
+    updateButton.setAttribute("data-bs-toggle","modal")
+    updateButton.setAttribute("data-bs-target","#exampleModal")
+    updateButton.addEventListener('click',(e)=>{
+        editInput.value=todo.name
+        changeid=todo.id
+    })
+
+    // delete button
+    const deleteButton=document.createElement('button')
+    deleteButton.classList.add('btn',"btn-danger")
+    deleteButton.innerHTML="deleteme"
+    deleteButton.addEventListener('click',(e)=>{
+        e.preventDefault()
+      todos=  todos.filter(el=>{
+            return el.id!==todo.id
+        })
+        displayTodos()
+    })
+        
+
+        // appending
+        buttonWrapper.append(updateButton)
+        buttonWrapper.append(deleteButton)
+        li.append(wrapme)
+        li.append(buttonWrapper)
+        todosList.append(li)
+    
+        
+      
+    })
+
+}
+
+savechangesbtn.addEventListener('click',(e)=>{
+    console.log(editInput.value);
+    todos.map((el)=>{
+        if(el.id===changeid){
+            el.name=editInput.value
+            return el
+        }else{
+            return el
+        }
+    })
+    displayTodos()
+    closebtn.click()
 
 })
-
-
-
-
-
-const updateme=(index)=>{
-    alert(index)
-    console.log(todos);
-}
-const deleteme=(li)=>{
-    let li=li.style.display="none"
-    return li
-    
-}
-
-
-// 0 1 2 3 4 
-// 0
-
-
-
-
-// logic 
-
-
-
-
-
-
-
-
-// // const createListElement=(text)=>{
-//     const task=document.createElement('span')
-//     const updatebutton=document.createElement('button')
-//     updatebutton.innerHTML="update"
-//     const deletebutton=document.createElement('button')
-//     deletebutton.innerHTML="delete"
-//     const li=document.createElement('li')
-//     task.innerText=text
-//     li.append(task)
-//     li.append(updatebutton)
-//     li.append(deletebutton)
-//     return li
-
-// }
-// const deleteTodos=(index)=>{
-
-
-// }
